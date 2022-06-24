bc6bc449ab37afb7116f18005169c6bc90fec482
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-06-23 21:25:54 -0300

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,5 +38,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(cors({
+  origin: '*'
+}));
 
 module.exports = app;
